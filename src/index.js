307f7135bb5301cb8d5fceaca434d12951aeaa74
Super Promiser <superpromiser@gmail.com>
2021-01-13 23:26:28 +0800

    props

--- src/index.js
+++ src/index.js
@@ -2,57 +2,29 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
+const author = "Amelia Hepworth";
+const title = "I Love You to the Moon and Back";
+const img = "http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_3.png";
 function BookList() {
   return (
     <section className="bookList">
       {/* <Person></Person>
       <Message></Message> */}
       <Book></Book>
-      <Book></Book>
-      <Book></Book>
-      <Book></Book>
-      <Book></Book>
-      <Book></Book>
     </section>
   )
 }
 
-const Person = () => {
-  return <h2>john doe</h2>
-}
-
-const Message = () => {
-  return <p>this is my message</p>
-}
-
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
   return (
     <article className="book">
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
+      <img src={img} alt=""></img>
+      <h1>{title}</h1>  
+      <h4>{author.toUpperCase()}</h4>
+      {console.log(props)}
     </article>
   )
 }
-const Image = () => {
-  return (
-    <img src="http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_3.png" alt=""></img>
-  )
-}
-
-const Title = () => {
-  return (
-    <h1>I Love You to the Moon and Back</h1>
-  );
-}
-
-const Author = () => {
-  return (
-    <h4 style={{color:'#617d98',fontSize:'0.75rem',margin:'0.25rem'}}>Amelia Hepworth</h4>
-  )
-}
-// const Greeting = () => {
-//   return React.createElement('h1',{},'hello world');
-// }
 
 ReactDom.render(<BookList></BookList>,document.getElementById('root'));
