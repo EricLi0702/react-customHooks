ef0f9e05bf058e8925af0c7f9cc341876a07bdd0
Super Promiser <superpromiser@gmail.com>
2021-01-13 16:32:34 +0800

    index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() 
-
-
-
-
-
-{
-  return (<h4> test</h4>)
+function Greeting() {
+  return <h4>hello world</h4>
 }
+
+// const Greeting = () => {
+//   return React.createElement('h1',{},'hello world');
+// }
 
 ReactDom.render(<Greeting></Greeting>,document.getElementById('root'));
